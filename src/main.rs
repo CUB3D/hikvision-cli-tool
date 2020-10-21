94cf933e26b9ec29f4efb0d291b45b94eb03a439
Callum Thomson <cub3d@Callums-Mini.lan>
2020-10-22 00:38:42 +0100

    Modify packet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
-use std::net::UdpSocket;
 use serde::{Deserialize, Serialize};
+use std::net::UdpSocket;
 use std::time::Duration;
 use uuid::Uuid;
 
+//Questions to answer:
+// What is the method for calculating the password
+// How does activation work
+// How does the password reset work
+
 #[derive(Deserialize, Serialize, Debug, Clone)]
-struct Probe {
-    #[serde(rename = "Uuid")]
-    uuid: String,
-    #[serde(rename = "Types")]
-    types: String,
+#[serde(tag = "Types")]
+enum Probe {
+    #[serde(rename = "Update")]
+    Update {
+        #[serde(rename = "Uuid")]
+        uuid: String,
+        #[serde(rename = "PWErrorParse")]
+        pw_error_parse: String,
+        #[serde(rename = "MAC")]
+        mac: String,
+        //TODO: how is this derived, base64(something(password))
+        #[serde(rename = "Password")]
+        password: String,
+        #[serde(rename = "IPv4Address")]
+        ipv4_address: String,
+        #[serde(rename = "CommandPort")]
+        command_port: String,
+        #[serde(rename = "HttpPort")]
+        http_port: String,
+        #[serde(rename = "IPv4SubnetMask")]
+        ipv4_subnet_mask: String,
+        #[serde(rename = "IPv4Gateway")]
+        ipv4_gateway: String,
+        #[serde(rename = "IPv6Address")]
+        ipv6_address: String,
+        #[serde(rename = "IPv6Gateway")]
+        ipv6_gateway: String,
+        #[serde(rename = "IPv6MaskLen")]
+        ipv6_mask_len: u32,
+        #[serde(rename = "DHCP")]
+        dhcp: bool,
+        #[serde(rename = "SDKOverTLSPort")]
+        sdk_over_tls_port: u32,
+    },
+    #[serde(rename = "inquiry")]
+    Inquiry {
+        #[serde(rename = "Uuid")]
+        uuid: String,
+    },
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -72,20 +111,41 @@ struct ProbeMatch {
     is_modify_verification_code: String,
 }
 
-
-
 fn main() {
-    let socket:UdpSocket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:0").unwrap();
     socket.set_broadcast(true).unwrap();
-    socket.set_read_timeout(Some(Duration::from_secs(5))).unwrap();
+    socket
+        .set_read_timeout(Some(Duration::from_secs(5)))
+        .unwrap();
 
-    let probe = Probe {
-        types: "inquiry".to_string(),
+    let probe = Probe::Inquiry {
         uuid: Uuid::new_v4().to_string(),
     };
 
+    // let probe = Probe::Update {
+    //     uuid: "".to_string(),
+    //     pw_error_parse: "".to_string(),
+    //     mac: "".to_string(),
+    //     password: "".to_string(),
+    //     ipv4_address: "".to_string(),
+    //     command_port: "".to_string(),
+    //     http_port: "".to_string(),
+    //     ipv4_subnet_mask: "".to_string(),
+    //     ipv4_gateway: "".to_string(),
+    //     ipv6_address: "".to_string(),
+    //     ipv6_gateway: "".to_string(),
+    //     ipv6_mask_len: 0,
+    //     dhcp: false,
+    //     sdk_over_tls_port: 0
+    // };
+
     println!("Starting discovery!");
-    socket.send_to(serde_xml_rs::to_string(&probe).unwrap().as_bytes(), "255.255.255.255:37020").unwrap();
+    socket
+        .send_to(
+            serde_xml_rs::to_string(&probe).unwrap().as_bytes(),
+            "255.255.255.255:37020",
+        )
+        .unwrap();
     let mut recv_buff = [0u8; 5000];
 
     println!("Desc\t\tip");
