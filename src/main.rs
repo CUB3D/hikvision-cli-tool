724d5a78bd8879ede60dea3173419a53e6a83fcf
CUB3D <callumthom11@gmail.com>
2020-10-28 23:56:50 +0000

    Support for sending update commands (only supports configuring dhcp so far)

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,40 @@ mod hv_client;
 mod types;
 
 use crate::hv_client::HvClient;
-use crate::types::{Password, Probe, ProbeMatch};
+use crate::types::{Password, Probe, ProbeMatchSuccessBody};
 use clap::{App, AppSettings, Arg, SubCommand};
 use cli_table::format::CellFormat;
 use cli_table::{Cell, Row, Table};
 use std::num::NonZeroU64;
 use std::time::Duration;
 use uuid::Uuid;
-use serde::Serialize;
 
 //Questions to answer:
 // How does activation work
 // How does the password reset work
 
-fn find_cameras(client: &HvClient, uuid: &Uuid) -> Vec<ProbeMatch> {
+fn find_cameras(client: &HvClient, uuid: &Uuid) -> Vec<ProbeMatchSuccessBody> {
     let probe = Probe::Inquiry {
         uuid: uuid.to_string(),
     };
     client
         .send_broadcast(&probe)
         .expect("Unable to send discovery broadcast");
+    wait_for_cameras(client)
+        .into_iter()
+        .map(|c| c.expect("Inquire failed"))
+        .collect()
+}
+
+fn wait_for_cameras(client: &HvClient) -> Vec<Result<ProbeMatchSuccessBody, String>> {
     let mut cameras = vec![];
     while let Some(probe_match) = client.read_packet() {
-        cameras.push(probe_match);
+        cameras.push(probe_match.payload());
     }
     cameras
 }
 
-fn do_inquire(client: &HvClient, uuid: &Uuid) {
-    println!("Starting discovery!");
-    let cameras = find_cameras(&client, &uuid);
-
+fn print_table(cameras: Vec<ProbeMatchSuccessBody>) -> Table {
     let title_format = CellFormat::builder().bold(true).build();
     let mut table_cells = vec![Row::new(vec![
         Cell::new("Description", title_format),
@@ -45,7 +48,6 @@ fn do_inquire(client: &HvClient, uuid: &Uuid) {
         Cell::new("IPv4 Subnet Mask", title_format),
         Cell::new("MAC Address", title_format),
         Cell::new("DSP Version", title_format),
-
         Cell::new("Boot Time", title_format),
         Cell::new("IPv6 Address", title_format),
         Cell::new("IPv6 Gateway", title_format),
@@ -76,7 +78,14 @@ fn do_inquire(client: &HvClient, uuid: &Uuid) {
             Cell::new(&probe_match.hc_platform_enable, Default::default()),
         ]));
     }
-    let table = Table::new(table_cells, Default::default()).unwrap();
+    Table::new(table_cells, Default::default()).unwrap()
+}
+
+fn do_inquire(client: &HvClient, uuid: &Uuid) {
+    println!("Starting discovery!");
+    let cameras = find_cameras(&client, &uuid);
+
+    let table = print_table(cameras);
     table.print_stdout().unwrap();
 }
 
@@ -101,14 +110,93 @@ fn main() {
                 }),
         )
         .subcommand(SubCommand::with_name("inquire").about("Find IP cameras on local network"))
+        .subcommand(
+            SubCommand::with_name("update")
+                .about("Change settings")
+                .arg(
+                    Arg::with_name("serial")
+                        .long("serial")
+                        .value_name("serial")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("dhcp")
+                        .long("dhcp")
+                        .required(false)
+                        .value_name("enabled")
+                        .possible_values(&["true", "false"]),
+                )
+                .arg(
+                    Arg::with_name("password")
+                        .long("password")
+                        .required(true)
+                        .value_name("password"),
+                ),
+        )
         .get_matches();
 
     let our_uuid = Uuid::new_v4();
 
+    let client = HvClient::new_with_timeout(Duration::from_secs(
+        matches.value_of("timeout").unwrap().parse().unwrap(),
+    ));
+
     if let Some(_matches) = matches.subcommand_matches("inquire") {
-        let client = HvClient::new_with_timeout(Duration::from_secs(
-            matches.value_of("timeout").unwrap().parse().unwrap(),
-        ));
         do_inquire(&client, &our_uuid);
+    }
+
+    if let Some(matches) = matches.subcommand_matches("update") {
+        let target_serial = matches.value_of("serial").unwrap();
+        println!("Searching for cameras");
+        let cams = find_cameras(&client, &our_uuid);
+
+        if let Some(camera) = cams.iter().find(|c| c.device_sn == target_serial) {
+            println!("Found target camera");
+
+            let dhcp = matches
+                .value_of("dhcp")
+                .map(|v| v == "true")
+                .unwrap_or(camera.dhcp);
+
+            let password = matches.value_of("password").expect("No password given");
+
+            let probe = Probe::Update {
+                uuid: our_uuid.to_string(),
+                pw_error_parse: "true".to_string(),
+                mac: camera.mac.clone(),
+                password: Password::hash(password),
+                ipv4_address: camera.ipv4_address.clone(),
+                command_port: camera.command_port.to_string(),
+                http_port: camera.http_port.clone(),
+                ipv4_subnet_mask: camera.ipv4_subnet_mask.clone(),
+                ipv4_gateway: camera.ipv4_gateway.clone(),
+                ipv6_address: camera.ipv6_address.clone(),
+                ipv6_gateway: camera.ipv6_gateway.clone(),
+                ipv6_mask_len: camera.ipv6_mask_len,
+                dhcp,
+                sdk_over_tls_port: 0,
+            };
+
+            client.send_broadcast(&probe).unwrap();
+
+            let camera = wait_for_cameras(&client)
+                .first()
+                .cloned()
+                .expect("Camera didn't respond in time");
+
+            match camera {
+                Ok(camera) => {
+                    if camera.device_sn != target_serial {
+                        println!("Incorrect device responded");
+                        return;
+                    }
+                    println!("Camera updated successfully, new config:");
+                    print_table(vec![camera]).print_stdout().unwrap()
+                }
+                Err(_msg) => println!("Failed to update camera settings, is the password correct?"),
+            }
+        } else {
+            println!("Target camera not found");
+        }
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -55,15 +55,11 @@ pub enum Probe {
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ProbeMatch {
-    #[serde(rename = "Uuid")]
-    pub uuid: String,
-    #[serde(rename = "Types")]
-    pub types: String,
+pub struct ProbeMatchSuccessBody {
     #[serde(rename = "DeviceType")]
     pub device_type: String,
     #[serde(rename = "DeviceDescription")]
-    pub(crate) device_description: String,
+    pub device_description: String,
     #[serde(rename = "DeviceSN")]
     pub device_sn: String,
     #[serde(rename = "CommandPort")]
@@ -113,4 +109,114 @@ pub struct ProbeMatch {
     pub hc_platform_enable: String,
     #[serde(rename = "IsModifyVerificationCode")]
     pub is_modify_verification_code: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub enum ProbeMatchResult {
+    #[serde(rename = "failed")]
+    Failed,
+    #[serde(rename = "success")]
+    Success,
+}
+
+//todo: error (<Result>String</><
+#[derive(Deserialize, Debug, Clone)]
+pub struct ProbeMatch {
+    #[serde(rename = "Uuid")]
+    pub uuid: String,
+    #[serde(rename = "Types")]
+    pub types: String,
+
+    #[serde(rename = "Result")]
+    result: Option<ProbeMatchResult>,
+    #[serde(rename = "DeviceType")]
+    device_type: Option<String>,
+    #[serde(rename = "DeviceDescription")]
+    device_description: Option<String>,
+    #[serde(rename = "DeviceSN")]
+    device_sn: Option<String>,
+    #[serde(rename = "CommandPort")]
+    command_port: Option<u32>,
+    #[serde(rename = "HttpPort")]
+    http_port: Option<String>,
+    #[serde(rename = "MAC")]
+    mac: Option<String>,
+    #[serde(rename = "IPv4Address")]
+    ipv4_address: Option<String>,
+    #[serde(rename = "IPv4SubnetMask")]
+    ipv4_subnet_mask: Option<String>,
+    #[serde(rename = "IPv4Gateway")]
+    ipv4_gateway: Option<String>,
+    #[serde(rename = "IPv6Address")]
+    ipv6_address: Option<String>,
+    #[serde(rename = "IPv6Gateway")]
+    ipv6_gateway: Option<String>,
+    #[serde(rename = "IPv6MaskLen")]
+    ipv6_mask_len: Option<u32>,
+    #[serde(rename = "DHCP")]
+    dhcp: Option<bool>,
+    #[serde(rename = "AnalogChannelNum")]
+    analog_channel_num: Option<u32>,
+    #[serde(rename = "DigitalChannelNum")]
+    digital_channel_num: Option<u32>,
+    #[serde(rename = "SoftwareVersion")]
+    software_version: Option<String>,
+    #[serde(rename = "DSPVersion")]
+    dsp_version: Option<String>,
+    #[serde(rename = "BootTime")]
+    boot_time: Option<String>,
+    #[serde(rename = "ResetAbility")]
+    reset_ability: Option<bool>,
+    #[serde(rename = "DiskNumber")]
+    disk_number: Option<u32>,
+    #[serde(rename = "Activated")]
+    activated: Option<bool>,
+    #[serde(rename = "PasswordResetAbility")]
+    password_reset_ability: Option<bool>,
+    #[serde(rename = "PasswordResetModeSecond")]
+    password_reset_mode_second: Option<bool>,
+    #[serde(rename = "SupportHCPlatform")]
+    support_hc_platform: Option<bool>,
+    #[serde(rename = "HCPlatformEnable")]
+    // "true" or "flase", good job hikvision
+    hc_platform_enable: Option<String>,
+    #[serde(rename = "IsModifyVerificationCode")]
+    is_modify_verification_code: Option<String>,
+}
+
+impl ProbeMatch {
+    pub fn payload(self) -> Result<ProbeMatchSuccessBody, String> {
+        if let Some(ProbeMatchResult::Failed) = self.result {
+            Err("Camera indicated failure".to_string())
+        } else {
+            Ok(ProbeMatchSuccessBody {
+                device_type: self.device_type.unwrap(),
+                device_description: self.device_description.unwrap(),
+                device_sn: self.device_sn.unwrap(),
+                command_port: self.command_port.unwrap(),
+                http_port: self.http_port.unwrap(),
+                mac: self.mac.unwrap(),
+                ipv4_address: self.ipv4_address.unwrap(),
+                ipv4_subnet_mask: self.ipv4_subnet_mask.unwrap(),
+                ipv4_gateway: self.ipv4_gateway.unwrap(),
+                ipv6_address: self.ipv6_address.unwrap(),
+                ipv6_gateway: self.ipv6_gateway.unwrap(),
+                ipv6_mask_len: self.ipv6_mask_len.unwrap(),
+                dhcp: self.dhcp.unwrap(),
+                analog_channel_num: self.analog_channel_num.unwrap(),
+                digital_channel_num: self.digital_channel_num.unwrap(),
+                software_version: self.software_version.unwrap(),
+                dsp_version: self.dsp_version.unwrap(),
+                boot_time: self.boot_time.unwrap(),
+                reset_ability: self.reset_ability.unwrap(),
+                disk_number: self.disk_number.unwrap(),
+                activated: self.activated.unwrap(),
+                password_reset_ability: self.password_reset_ability.unwrap(),
+                password_reset_mode_second: self.password_reset_mode_second.unwrap(),
+                support_hc_platform: self.support_hc_platform.unwrap(),
+                hc_platform_enable: self.hc_platform_enable.unwrap(),
+                is_modify_verification_code: self.is_modify_verification_code.unwrap(),
+            })
+        }
+    }
 }
